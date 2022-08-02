ebd172af61b6cc7cddd2821a2a180741c068031d
Cole Bemis <colebemis@github.com>
2022-08-01 17:36:30 -0700

    Use iA Writer font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,10 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      body: ['"iA Writer Quattro"', "system-ui", "sans-serif"],
+      mono: ['"iA Writer Mono"', "monospace"],
+    },
   },
   plugins: [],
 };
