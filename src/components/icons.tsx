62224a3365494b70ae9fc819b43b64b76ced75ba
Cole Bemis <colebemis@github.com>
2022-08-10 17:58:50 -0700

    Fix icons jsx

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -2,8 +2,8 @@ export function MoreIcon16() {
   return (
     <svg width="16" height="16" viewBox="0 0 16 16" fill="currentColor">
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M8 9a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3zM1.5 9a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3zm13 0a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z"
       />
     </svg>
@@ -14,8 +14,8 @@ export function NoteIcon24() {
   return (
     <svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M4 4.5h16A1.5 1.5 0 0 1 21.5 6v12a1.5 1.5 0 0 1-1.5 1.5H4A1.5 1.5 0 0 1 2.5 18V6A1.5 1.5 0 0 1 4 4.5ZM1 6a3 3 0 0 1 3-3h16a3 3 0 0 1 3 3v12a3 3 0 0 1-3 3H4a3 3 0 0 1-3-3V6Zm6 3h10v1.5H7V9Zm0 4h7v1.5H7V13Z"
       />
     </svg>
@@ -32,8 +32,8 @@ export function DisconnectIcon24() {
       aria-hidden
     >
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M5 3.5h8A1.5 1.5 0 0 1 14.5 5v4H16V5a3 3 0 0 0-3-3H5a3 3 0 0 0-3 3v14a3 3 0 0 0 3 3h8a3 3 0 0 0 3-3v-4.5h-1.5V19a1.5 1.5 0 0 1-1.5 1.5H5A1.5 1.5 0 0 1 3.5 19V5A1.5 1.5 0 0 1 5 3.5ZM21.19 11H9v1.5h12.19l-3.22 3.22 1.06 1.06 4.5-4.5a.75.75 0 0 0 0-1.06l-4.5-4.5-1.06 1.06L21.19 11Z"
       />
     </svg>
