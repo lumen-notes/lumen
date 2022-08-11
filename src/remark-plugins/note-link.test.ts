02cdde0688437f9069894cd1bd9f5d22e426298c
Cole Bemis <colebemis@github.com>
2022-08-10 23:46:04 -0700

    Add more test cases

--- src/remark-plugins/note-link.test.ts
+++ src/remark-plugins/note-link.test.ts
@@ -15,6 +15,54 @@ function runTests(tests: Array<{ input: string; output: string }>) {
 }
 
 runTests([
+  // Valid
+  {
+    input: `[[123]]`,
+    output: `<p><note-link id="123" text="123" /></p>`,
+  },
+  {
+    input: `[[[123]]]`,
+    output: `<p>[<note-link id="123" text="123" />]</p>`,
+  },
+  {
+    input: `[[[[123]]]]`,
+    output: `<p>[[<note-link id="123" text="123" />]]</p>`,
+  },
+  {
+    input: `\`\`\`
+[[123]]
+\`\`\``,
+    output: `<pre><code>[[123]]
+</code></pre>`,
+  },
+  {
+    input: `\`[[123]]\``,
+    output: `<p><code>[[123]]</code></p>`,
+  },
+  {
+    input: `_[[123]]_`,
+    output: `<p><em><note-link id="123" text="123" /></em></p>`,
+  },
+  {
+    input: `- [[123]]`,
+    output: `<ul>
+<li><note-link id="123" text="123" /></li>
+</ul>`,
+  },
+  {
+    input: `[[123|hello]]`,
+    output: `<p><note-link id="123" text="hello" /></p>`,
+  },
+  {
+    input: `[[123|hello world]]`,
+    output: `<p><note-link id="123" text="hello world" /></p>`,
+  },
+  {
+    input: `[[123|hello]] [[456]]`,
+    output: `<p><note-link id="123" text="hello" /> <note-link id="456" text="456" /></p>`,
+  },
+
+  // Invalid
   {
     input: `hello`,
     output: `<p>hello</p>`,
@@ -32,11 +80,35 @@ runTests([
     output: `<p>[[]]</p>`,
   },
   {
-    input: `[[123]]`,
-    output: `<p><note-link id="123" text="123" /></p>`,
-  },
-  {
     input: `[[123x]]`,
     output: `<p>[[123x]]</p>`,
+  },
+  {
+    input: `[[x]]`,
+    output: `<p>[[x]]</p>`,
+  },
+  {
+    input: `[[123]`,
+    output: `<p>[[123]</p>`,
+  },
+  {
+    input: `[[123`,
+    output: `<p>[[123</p>`,
+  },
+  {
+    input: `[123]]`,
+    output: `<p>[123]]</p>`,
+  },
+  {
+    input: `123]]`,
+    output: `<p>123]]</p>`,
+  },
+  {
+    input: `[123]`,
+    output: `<p>[123]</p>`,
+  },
+  {
+    input: `[[123|]]`,
+    output: `<p>[[123|]]</p>`,
   },
 ])
