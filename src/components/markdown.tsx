a6e412d612cc01ca92896b648f81bfc2a446643a
Cole Bemis <colebemis@github.com>
2022-08-26 17:44:02 -0700

    Implement hovercard

--- src/components/markdown.tsx
+++ src/components/markdown.tsx
@@ -1,11 +1,15 @@
+import * as HoverCard from "@radix-ui/react-hover-card"
+import { useActor } from "@xstate/react"
 import React from "react"
 import ReactMarkdown from "react-markdown"
 import { Link } from "react-router-dom"
 import remarkGfm from "remark-gfm"
+import { GlobalStateContext } from "../global-state"
 import { remarkDateLink } from "../remark-plugins/date-link"
 import { remarkNoteLink } from "../remark-plugins/note-link"
 import { remarkTagLink } from "../remark-plugins/tag-link"
 import { formatDate } from "../utils/date"
+import { Card } from "./card"
 
 type MarkdownProps = {
   children: string
@@ -39,9 +43,7 @@ export const Markdown = React.memo(({ children }: MarkdownProps) => {
       }}
       components={{
         // @ts-ignore I'm not sure how to extend the list of accepted component keys
-        noteLink({ id, text }) {
-          return <Link to={`/${id}`}>{text}</Link>
-        },
+        noteLink: NoteLink,
         // @ts-ignore
         tagLink({ name }) {
           return (
@@ -60,3 +62,26 @@ export const Markdown = React.memo(({ children }: MarkdownProps) => {
     </ReactMarkdown>
   )
 })
+
+type NoteLinkProps = {
+  id: string
+  text: string
+}
+
+function NoteLink({ id, text }: NoteLinkProps) {
+  const globalState = React.useContext(GlobalStateContext)
+  const [state] = useActor(globalState.service)
+  const body = state.context.notes[id]
+  return (
+    <HoverCard.Root>
+      <HoverCard.Trigger asChild>
+        <Link to={`/${id}`}>{text}</Link>
+      </HoverCard.Trigger>
+      <HoverCard.Content side="top" sideOffset={4} asChild>
+        <Card className="w-96 px-4 py-3" elevation={1}>
+          <Markdown>{body ?? "Not found"}</Markdown>
+        </Card>
+      </HoverCard.Content>
+    </HoverCard.Root>
+  )
+}
