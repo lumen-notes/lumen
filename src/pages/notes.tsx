de9ceb539213fcc0eb56344709584f1926afe324
Cole Bemis <colebemis@github.com>
2022-09-09 18:52:43 -0700

    Create notes panel

--- src/pages/notes.tsx
+++ src/pages/notes.tsx
@@ -1,27 +1,12 @@
-import { useActor } from "@xstate/react"
-import React from "react"
 import { CommandMenu } from "../components/command-menu"
-import { NoteIcon24 } from "../components/icons"
-import { NoteForm } from "../components/note-form"
-import { NoteList } from "../components/note-list"
-import { Panel } from "../components/panel"
 import { Panels } from "../components/panels"
-import { GlobalStateContext } from "../global-state"
+import { NotesPanel } from "../panels/notes"
 
 export function NotesPage() {
-  const globalState = React.useContext(GlobalStateContext)
-  const [state] = useActor(globalState.service)
-  const noteIds = Object.keys(state.context.notes)
-
   return (
     <Panels>
       <CommandMenu />
-      <Panel title="Notes" icon={<NoteIcon24 />}>
-        <div className="flex flex-col gap-4 px-4 pb-4">
-          <NoteForm />
-          <NoteList ids={noteIds} />
-        </div>
-      </Panel>
+      <NotesPanel />
       <Panels.Outlet />
     </Panels>
   )
---
+++ src/panels/notes.tsx
@@ -0,0 +1,23 @@
+import { useActor } from "@xstate/react"
+import React from "react"
+import { NoteIcon24 } from "../components/icons"
+import { NoteForm } from "../components/note-form"
+import { NoteList } from "../components/note-list"
+import { Panel } from "../components/panel"
+import { PanelProps } from "../components/panels"
+import { GlobalStateContext } from "../global-state"
+
+export function NotesPanel({ id, onClose }: PanelProps) {
+  const globalState = React.useContext(GlobalStateContext)
+  const [state] = useActor(globalState.service)
+  const noteIds = Object.keys(state.context.notes)
+
+  return (
+    <Panel id={id} title="Notes" icon={<NoteIcon24 />} onClose={onClose}>
+      <div className="flex flex-col gap-4 px-4 pb-4">
+        <NoteForm />
+        <NoteList ids={noteIds} />
+      </div>
+    </Panel>
+  )
+}
