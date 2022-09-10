7ff6af600535590232fde8768db3c7886ff3e95e
Cole Bemis <colebemis@github.com>
2022-09-09 18:49:22 -0700

    Create tags panel

---
+++ src/panels/tags.tsx
@@ -0,0 +1,72 @@
+import { useActor } from "@xstate/react"
+import { Searcher } from "fast-fuzzy"
+import React from "react"
+import { z } from "zod"
+import { TagIcon24 } from "../components/icons"
+import { Panel } from "../components/panel"
+import { PanelProps, Panels } from "../components/panels"
+import { SearchInput } from "../components/search-input"
+import { GlobalStateContext } from "../global-state"
+import { pluralize } from "../utils/pluralize"
+import { useDebounce } from "../utils/use-debounce"
+import { useSearchParam } from "../utils/use-search-param"
+
+export function TagsPanel({ id, onClose }: PanelProps) {
+  const globalState = React.useContext(GlobalStateContext)
+  const [state] = useActor(globalState.service)
+
+  const [query, setQuery] = useSearchParam("q", {
+    defaultValue: "",
+    schema: z.string(),
+    replace: true,
+  })
+
+  const debouncedQuery = useDebounce(query)
+
+  // Sort tags alphabetically
+  const sortedTags = React.useMemo(
+    () => Object.entries(state.context.tags).sort((a, b) => a[0].localeCompare(b[0])),
+    [state.context.tags],
+  )
+
+  // Create a search index
+  const searcher = React.useMemo(() => {
+    return new Searcher(sortedTags, {
+      keySelector: ([name]) => name,
+      threshold: 0.8,
+    })
+  }, [sortedTags])
+
+  const results = React.useMemo(() => {
+    if (!debouncedQuery) {
+      return sortedTags
+    }
+
+    return searcher.search(debouncedQuery)
+  }, [debouncedQuery, sortedTags, searcher])
+
+  return (
+    <Panel id={id} title="Tags" icon={<TagIcon24 />} onClose={onClose}>
+      <div className="flex flex-col gap-2 px-4 pb-4">
+        <SearchInput
+          placeholder={`Search ${pluralize(sortedTags.length, "tag")}`}
+          value={query}
+          onChange={(event) => setQuery(event.target.value)}
+        />
+        <ul className="flex flex-col">
+          {results.map(([name, noteIds]) => (
+            <li
+              key={name}
+              className="flex justify-between border-b border-border-divider py-3 last:border-b-0"
+            >
+              <Panels.Link className="underline underline-offset-2" to={`/tags/${name}`}>
+                #{name}
+              </Panels.Link>
+              <span className="text-text-muted">{pluralize(noteIds.length, "note")}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </Panel>
+  )
+}
