6105b00b99f4b987b181e406becf5850f1f08873
Cole Bemis <colebemis@github.com>
2022-09-13 20:10:59 -0700

    update useDebounce to useTransition

--- src/utils/use-debounce.tsx
+++ src/utils/use-debounce.tsx
@@ -1,16 +1,11 @@
 import React from "react"
 
-// Copied from https://usehooks-ts.com/react-hook/use-debounce
-export function useDebounce<T>(value: T, delay: number = 50): T {
+export function useDebounce<T>(value: T): T {
   const [debouncedValue, setDebouncedValue] = React.useState<T>(value)
 
   React.useEffect(() => {
-    const timer = setTimeout(() => setDebouncedValue(value), delay)
-
-    return () => {
-      clearTimeout(timer)
-    }
-  }, [value, delay])
+    React.startTransition(() => setDebouncedValue(value))
+  }, [value])
 
   return debouncedValue
 }
