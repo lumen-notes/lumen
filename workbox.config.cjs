6da4ad20f1c4681f33abc460fd47cee458b1497b
Cole Bemis <colebemis@github.com>
2022-09-18 17:59:37 -0700

    Use network first caching strategy

--- workbox.config.cjs
+++ workbox.config.cjs
@@ -1,6 +1,12 @@
 module.exports = {
   globDirectory: "dist/",
   globPatterns: ["**/*.{css,js,woff2}"],
+  runtimeCaching: [
+    {
+      urlPattern: ({ request }) => request.mode === "navigate",
+      handler: "NetworkFirst",
+    },
+  ],
   swDest: "dist/sw.js",
   ignoreURLParametersMatching: [/^utm_/, /^fbclid$/],
 }
