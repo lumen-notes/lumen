b5c627958b59616974787c3cf555a49b581949cc
Cole Bemis <colebemis@github.com>
2022-09-23 18:35:53 -0700

    Update search style

--- src/components/search-input.tsx
+++ src/components/search-input.tsx
@@ -1,21 +1,20 @@
 import React from "react"
-import { Card } from "./card"
 import { SearchIcon16 } from "./icons"
 
 type SearchInputProps = React.ComponentPropsWithoutRef<"input">
 
 export const SearchInput = React.forwardRef<HTMLInputElement, SearchInputProps>((props, ref) => {
   return (
-    <Card className="relative">
+    <div className="relative">
       <div className="absolute top-0 bottom-0 left-4 flex items-center text-text-muted">
         <SearchIcon16 />
       </div>
       <input
         ref={ref}
-        className="w-full rounded-lg bg-transparent px-4 py-3 pl-[44px] [font-variant-numeric:inherit] placeholder:text-text-placeholder"
+        className="w-full rounded-lg bg-bg-hover px-4 py-3 pl-[44px] [font-variant-numeric:inherit] placeholder:text-text-placeholder focus:bg-bg"
         type="search"
         {...props}
       />
-    </Card>
+    </div>
   )
 })
