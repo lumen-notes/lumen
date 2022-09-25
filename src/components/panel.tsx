4212bf322928ce1e813de1e7d110188201ae32c6
Cole Bemis <colebemis@github.com>
2022-09-25 12:36:13 -0700

    Resize panels
    Closes #1

--- src/components/panel.tsx
+++ src/components/panel.tsx
@@ -1,5 +1,6 @@
 import clsx from "clsx"
 import React from "react"
+import { DraggableCore } from "react-draggable"
 import { IconButton } from "./button"
 import { CloseIcon16 } from "./icons"
 
@@ -12,7 +13,12 @@ type PanelProps = {
   onClose?: () => void
 }
 
+const MIN_WIDTH = 512
+const MAX_WIDTH = 800
+
 export function Panel({ id, title, description, icon, children, onClose }: PanelProps) {
+  const [isResizing, setIsResizing] = React.useState(false)
+  const [width, setWidth] = React.useState(MIN_WIDTH)
   const panelRef = React.useRef<HTMLDivElement>(null)
   return (
     // eslint-disable-next-line jsx-a11y/no-static-element-interactions
@@ -22,7 +28,8 @@ export function Panel({ id, title, description, icon, children, onClose }: Panel
       // eslint-disable-next-line jsx-a11y/no-noninteractive-tabindex
       tabIndex={0}
       id={id}
-      className="flex h-full w-[80vw] max-w-lg flex-shrink-0 flex-col overflow-auto border-r border-border-divider focus:outline-none"
+      className="relative h-full w-[80vw] flex-shrink-0 border-r border-border-divider focus:outline-none"
+      style={{ maxWidth: width }}
       onKeyDown={(event) => {
         // Close with `command + x` if no text is selected
         if (event.metaKey && event.key === "x" && !window.getSelection()?.toString()) {
@@ -41,25 +48,54 @@ export function Panel({ id, title, description, icon, children, onClose }: Panel
         }
       }}
     >
+      {/* TODO: Resize with keyboard */}
+      <DraggableCore
+        onStart={() => setIsResizing(true)}
+        onStop={() => setIsResizing(false)}
+        onDrag={(event, { deltaX }) => {
+          setWidth((width) => clamp(width + deltaX, MIN_WIDTH, MAX_WIDTH))
+          event.preventDefault()
+        }}
+      >
+        <div
+          data-resizing={isResizing}
+          className={clsx(
+            "absolute top-0 bottom-0 right-0 z-20 w-1 cursor-col-resize",
+            !isResizing && "hover:bg-bg-hover",
+            isResizing && "bg-border-divider",
+          )}
+        />
+      </DraggableCore>
       <div
         className={clsx(
-          "sticky top-0 z-10 flex h-[3.5rem] shrink-0 items-center justify-between gap-2 border-b border-border-divider bg-bg-backdrop p-4 backdrop-blur-md",
+          "flex h-full flex-col overflow-auto",
+          // isResizing && "[&::-webkit-scrollbar]:hidden",
         )}
       >
-        <div className="flex flex-shrink gap-2">
-          <div className="flex-shrink-0">{icon}</div>
-          <div className="flex items-baseline gap-3">
-            <h2 className="flex-shrink-0 text-lg font-semibold leading-6">{title}</h2>
-            {description ? <span className="truncate text-text-muted">{description}</span> : null}
+        <div
+          className={clsx(
+            "sticky top-0 z-10 flex h-[3.5rem] shrink-0 items-center justify-between gap-2 border-b border-border-divider bg-bg-backdrop p-4 backdrop-blur-md",
+          )}
+        >
+          <div className="flex flex-shrink gap-2">
+            <div className="flex-shrink-0">{icon}</div>
+            <div className="flex items-baseline gap-3">
+              <h2 className="flex-shrink-0 text-lg font-semibold leading-6">{title}</h2>
+              {description ? <span className="truncate text-text-muted">{description}</span> : null}
+            </div>
           </div>
+          {onClose ? (
+            <IconButton aria-label="Close panel" shortcut="⌘X" onClick={() => onClose()}>
+              <CloseIcon16 />
+            </IconButton>
+          ) : null}
         </div>
-        {onClose ? (
-          <IconButton aria-label="Close panel" shortcut="⌘X" onClick={() => onClose()}>
-            <CloseIcon16 />
-          </IconButton>
-        ) : null}
+        <div className="flex-grow p-4">{children}</div>
       </div>
-      <div className="flex-grow p-4">{children}</div>
     </div>
   )
+}
+
+function clamp(value: number, min: number, max: number) {
+  return Math.min(Math.max(value, min), max)
 }
