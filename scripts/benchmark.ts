37c478fb12957900c3c361e3628ccadce4969fa6
Cole Bemis <colebemis@github.com>
2022-10-19 23:52:44 -0700

    Add benchmark test for markdown parsing

---
+++ scripts/benchmark.ts
@@ -0,0 +1,83 @@
+import Benchmark from "benchmark"
+import { fromMarkdown } from "mdast-util-from-markdown"
+import { dateLink, dateLinkFromMarkdown } from "../src/remark-plugins/date-link"
+import { noteLink, noteLinkFromMarkdown } from "../src/remark-plugins/note-link"
+import { tagLink, tagLinkFromMarkdown } from "../src/remark-plugins/tag-link"
+
+const markdown = `
+# Heading 1
+## Heading 2
+### Heading 3
+#### Heading 4
+##### Heading 5
+###### Heading 6
+
+---
+
+Paragraph with **bold** and *italic* and [link](https://example.com).
+
+- List item 1
+- List item 2
+- List item 3
+
+1. List item 1
+2. List item 2
+3. List item 3
+
+> Blockquote
+
+\`\`\`js
+const foo = "bar"
+\`\`\`
+
+\`inline code\`
+
+![image](https://example.com/image.png)
+
+| Table | Header |
+| ----- | ------ |
+| Cell  | Cell   |
+
+[[123456789|Note link]]
+
+[[1998-7-11]]
+
+#tag
+`
+
+const suite = new Benchmark.Suite("Markdown parsing")
+
+suite.add("Markdown without syntax extensions", () => {
+  fromMarkdown(markdown)
+})
+
+suite.add("Markdown with syntax extensions", () => {
+  fromMarkdown(markdown, {
+    extensions: [noteLink(), tagLink(), dateLink()],
+    mdastExtensions: [noteLinkFromMarkdown(), tagLinkFromMarkdown(), dateLinkFromMarkdown()],
+  })
+})
+
+suite.on("complete", function () {
+  const fastest: Benchmark = this.filter("fastest")[0]
+
+  this.forEach((benchmark: Benchmark) => {
+    const isFastest = benchmark === fastest
+    console.log(
+      `${benchmark.name}: ${Benchmark.formatNumber(Math.round(benchmark.hz))} ops/sec`,
+      isFastest ? `(fastest)` : `(${percentDecrease(fastest.hz, benchmark.hz)}% slower)`,
+    )
+  })
+})
+
+/** Calculate the percent decrease between two numbers */
+function percentDecrease(a: number, b: number): number {
+  return round(((a - b) / a) * 100, 2)
+}
+
+/** Round to the nearest given number of decimal places */
+function round(value: number, decimals: number): number {
+  return Number(Math.round(Number(value + "e" + decimals)) + "e-" + decimals)
+}
+
+suite.run()
