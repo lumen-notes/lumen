725404986dc6da4ded4e68cc1f241d38bb5b03cb
Cole Bemis <colebemis@github.com>
2022-11-03 21:28:09 -0700

    Set up graph data structures

--- src/pages/graph.tsx
+++ src/pages/graph.tsx
@@ -1,3 +1,60 @@
+import React from "react"
+import { GlobalStateContext } from "../global-state"
+import { useActor } from "@xstate/react"
+
+type NodeType = "note" | "tag" | "date"
+
+type Node = {
+  id: string
+  type: NodeType
+}
+
+type Link = {
+  source: string
+  target: string
+}
+
 export function GraphPage() {
+  const globalState = React.useContext(GlobalStateContext)
+  const [state] = useActor(globalState.service)
+
+  const nodes = [
+    ...state.context.sortedNoteIds.map<Node>((noteId) => ({
+      type: "note",
+      id: noteId,
+    })),
+    ...Object.keys(state.context.tags).map<Node>((tagName) => ({
+      type: "tag",
+      id: tagName,
+    })),
+    ...Object.keys(state.context.dates).map<Node>((date) => ({
+      type: "date",
+      id: date,
+    })),
+  ]
+
+  const links = [
+    ...Object.entries(state.context.backlinks).flatMap<Link>(([noteId, backlinks]) =>
+      backlinks.map((backlink) => ({
+        source: noteId,
+        target: backlink,
+      })),
+    ),
+    ...Object.entries(state.context.tags).flatMap<Link>(([tagName, noteIds]) =>
+      noteIds.map((noteId) => ({
+        source: tagName,
+        target: noteId,
+      })),
+    ),
+    ...Object.entries(state.context.dates).flatMap<Link>(([date, noteIds]) =>
+      noteIds.map((noteId) => ({
+        source: date,
+        target: noteId,
+      })),
+    ),
+  ]
+
+  console.log({ nodes, links })
+
   return <div>Graph</div>
 }
