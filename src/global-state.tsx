97fbd8cd414407e49ca804f80e9a2af49c4f641a
Cole Bemis <colebemis@github.com>
2022-11-18 15:41:54 -0800

    Update src/global-state.tsx

--- src/global-state.tsx
+++ src/global-state.tsx
@@ -459,7 +459,6 @@ const machine = createMachine(
           worker.postMessage({ directoryHandle: context.directoryHandle })
           worker.onmessage = (event) => {
             resolve(event.data)
-            // worker.terminate()
           }
         })
       },
