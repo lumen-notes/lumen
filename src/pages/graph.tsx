7242fff01002981b81209e306ecba4cb61a44c60
Cole Bemis <colebemis@github.com>
2022-12-03 07:01:02 -0800

    Click on links to navigate graph

--- src/pages/graph.tsx
+++ src/pages/graph.tsx
@@ -1,12 +1,21 @@
 import { useActor } from "@xstate/react"
 import Graph from "graphology"
 import React from "react"
+import { Link as RouterLink, LinkProps } from "react-router-dom"
 import { useMeasure } from "react-use"
 import { z } from "zod"
-import { NetworkGraph } from "../components/network-graph"
+import { LinkContext } from "../components/link-context"
+import { NetworkGraph, NetworkGraphInstance } from "../components/network-graph"
 import { NoteCard } from "../components/note-card"
 import { GlobalStateContext } from "../global-state"
 import { useSearchParam } from "../utils/use-search-param"
+
+const GraphContext = React.createContext({
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  selectNode: (nodeId: string) => {},
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  hoverNode: (nodeId: string) => {},
+})
 
 export function GraphPage() {
   const [selectedId, setSelectedId] = useSearchParam("id", {
@@ -46,23 +55,76 @@ export function GraphPage() {
 
   const [ref, { width, height }] = useMeasure<HTMLDivElement>()
 
+  const graphRef = React.useRef<NetworkGraphInstance>(null)
+
+  const selectNode = React.useCallback(
+    (id: string) => {
+      setSelectedId(id)
+      graphRef.current?.focus()
+    },
+    [setSelectedId],
+  )
+
+  const contextValue = React.useMemo(
+    () => ({ selectNode, hoverNode: setHoveredId }),
+    [selectNode, setHoveredId],
+  )
+
   return (
-    <div ref={ref} className="relative h-full w-full overflow-hidden">
-      <NetworkGraph
-        width={width}
-        height={height}
-        nodes={nodes}
-        links={links}
-        selectedId={selectedId}
-        hoveredId={hoveredId}
-        onSelect={(node) => setSelectedId(node?.id || "")}
-        onHover={(node) => setHoveredId(node?.id || "")}
-      />
-      {selectedId ? (
-        <div className="absolute bottom-0 right-0 max-h-full w-full max-w-md overflow-auto p-4">
-          <NoteCard id={selectedId} />
+    <GraphContext.Provider value={contextValue}>
+      <LinkContext.Provider value={Link}>
+        <div ref={ref} className="relative h-full w-full overflow-hidden">
+          <NetworkGraph
+            ref={graphRef}
+            width={width}
+            height={height}
+            nodes={nodes}
+            links={links}
+            selectedId={selectedId}
+            hoveredId={hoveredId}
+            onSelect={(node) => setSelectedId(node?.id || "")}
+            onHover={(node) => setHoveredId(node?.id || "")}
+          />
+          {selectedId ? (
+            <div className="absolute bottom-0 right-0 max-h-full w-full max-w-md overflow-auto p-4">
+              <NoteCard id={selectedId} />
+            </div>
+          ) : null}
         </div>
-      ) : null}
-    </div>
+      </LinkContext.Provider>
+    </GraphContext.Provider>
   )
 }
+
+const Link = React.forwardRef<HTMLAnchorElement, LinkProps>((props, ref) => {
+  const { selectNode, hoverNode } = React.useContext(GraphContext)
+  return (
+    <RouterLink
+      {...props}
+      ref={ref}
+      onClick={(event) => {
+        if (typeof props.to !== "string" || event.metaKey || event.ctrlKey || event.shiftKey) {
+          return
+        }
+
+        // Note link
+        if (props.to.match(/^\/\d+$/)) {
+          const id = props.to.replace(/^\//, "")
+          selectNode(id)
+          event.preventDefault()
+        }
+      }}
+      onMouseEnter={(event) => {
+        if (typeof props.to !== "string") return
+
+        // Note link
+        if (props.to.match(/^\/\d+$/)) {
+          const id = props.to.replace(/^\//, "")
+          hoverNode(id)
+          event.preventDefault()
+        }
+      }}
+      onMouseLeave={() => hoverNode("")}
+    />
+  )
+})
