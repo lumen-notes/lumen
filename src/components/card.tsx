a9b17f296a5ca3340da00cb74ab44413f9c7dd21
Cole Bemis <colebemis@github.com>
2023-01-05 13:36:13 -0700

    Fix focus outline

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -12,10 +12,12 @@ export const Card = React.forwardRef<HTMLDivElement, CardProps>(
       <div
         ref={ref}
         className={clsx(
-          "relative rounded-lg ring-1 ring-border-secondary after:pointer-events-none after:absolute after:inset-0 after:rounded-lg focus:outline-0 focus:after:outline focus:after:outline-2 focus:after:-outline-offset-1 focus:after:outline-border-focus dark:ring-0 dark:after:ring-1 dark:after:ring-inset dark:after:ring-border-secondary",
+          "relative rounded-lg ring-1 ring-border-secondary after:pointer-events-none after:absolute after:inset-0 after:rounded-lg focus:outline-0  dark:ring-0 dark:after:ring-1 dark:after:ring-inset dark:after:ring-border-secondary",
           elevation === 0 && "bg-bg shadow-sm",
           elevation === 1 && "bg-bg-overlay shadow-lg",
           elevation === 2 && "bg-bg-overlay shadow-xl",
+          props.tabIndex === 0 &&
+            "focus:after:outline focus:after:outline-2 focus:after:-outline-offset-1 focus:after:outline-border-focus",
           focusVisible &&
             "after:outline after:outline-2 after:-outline-offset-1 after:outline-border-focus",
           className,
