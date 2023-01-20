7c14fc61ef438b78f4e064ae4a9da171b69432da
Cole Bemis <colebemis@github.com>
2023-01-20 12:23:39 -0700

    Hover only when supported

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,9 @@ const plugin = require("tailwindcss/plugin")
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
   theme: {
     fontFamily: {
       body: ['"iA Writer Quattro"', "system-ui", "sans-serif"],
