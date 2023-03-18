f56fb1506311b9f566428260144ae5fd8ae9ebe7
Cole Bemis <colebemis@github.com>
2023-03-17 20:47:08 -0600

    Write parseFrontmatter function

---
+++ src/utils/parse-frontmatter.ts
@@ -0,0 +1,20 @@
+import { parse } from "yaml"
+
+const FRONTMATTER_REGEX = /^---\n([\s\S]*?)\n---\n([\s\S]*)$/
+
+/** Parses frontmatter from a markdown string */
+export function parseFrontmatter(markdown: string): {
+  frontmatter: Record<string, unknown>
+  content: string
+} {
+  const match = markdown.match(FRONTMATTER_REGEX)
+
+  if (!match) {
+    return { frontmatter: {}, content: markdown }
+  }
+
+  const [, frontmatterYaml, content] = match
+  const frontmatter = parse(frontmatterYaml)
+
+  return { frontmatter, content }
+}
