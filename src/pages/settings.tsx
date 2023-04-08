7c85901e44d33aee6b90f5fc045e2fdeff439138
Cole Bemis <colebemis@github.com>
2023-04-08 00:19:15 -0600

    Create GitHubAuth component

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -1,20 +1,20 @@
-import { useAtom } from "jotai"
+import { atom, useAtom, useAtomValue } from "jotai"
 import { loadable } from "jotai/utils"
 import React from "react"
 import { useNavigate } from "react-router-dom"
+import { z } from "zod"
 import { Button } from "../components/button"
 import { Card } from "../components/card"
 import { CommandMenu } from "../components/command-menu"
 import { SettingsIcon24 } from "../components/icons"
 import { Input } from "../components/input"
 import { Panel } from "../components/panel"
-import { githubRepoAtom, githubTokenAtom, githubUserAtom } from "../global-atoms"
+import { githubRepoAtom, githubTokenAtom } from "../global-atoms"
 import { useFetchNotes } from "../utils/github-sync"
 
 export function SettingsPage() {
-  const [githubToken, setGitHubToken] = useAtom(githubTokenAtom)
+  const [githubToken] = useAtom(githubTokenAtom)
   const [githubRepo, setGitHubRepo] = useAtom(githubRepoAtom)
-  const [githubUser] = useAtom(React.useMemo(() => loadable(githubUserAtom), []))
   const { fetchNotes } = useFetchNotes()
   const navigate = useNavigate()
   return (
@@ -27,44 +27,27 @@ export function SettingsPage() {
               <h3 className="text-lg font-semibold !leading-none">GitHub</h3>
               <p>Store your notes as Markdown files in a GitHub repository of your choice.</p>
             </div>
-            {githubUser.state === "hasData" && githubUser.data ? (
-              <div className="flex items-center justify-between rounded-sm bg-bg-secondary p-4">
-                <div className="flex items-center gap-3">
-                  <div
-                    aria-hidden
-                    className="inline-block h-8 w-8 rounded-full bg-bg-secondary bg-cover ring-1 ring-inset ring-border-secondary coarse:h-10 coarse:w-10"
-                    style={{
-                      backgroundImage: `url(${githubUser.data.avatar_url})`,
-                    }}
-                  />
-                  <div className="flex flex-col gap-1">
-                    <span className="text-sm leading-3 coarse:leading-4">Signed in as</span>
-                    <span className="font-semibold leading-4 coarse:leading-5">
-                      {githubUser.data.login}
-                    </span>
-                  </div>
-                </div>
-                {/* <Button onClick={() => setGitHubToken("")}>Sign out</Button> */}
-              </div>
-            ) : null}
-            <form
-              id="github-form"
-              className="flex flex-col gap-4"
-              onSubmit={(event) => {
-                event.preventDefault()
-                const formData = new FormData(event.currentTarget)
-                const token = String(formData.get("token"))
-                const repoOwner = String(formData.get("repo-owner"))
-                const repoName = String(formData.get("repo-name"))
+            <GitHubAuth />
+            {githubToken ? (
+              <div className="grid gap-5">
+                <form
+                  id="github-form"
+                  className="flex flex-col gap-4"
+                  onSubmit={(event) => {
+                    event.preventDefault()
+                    const formData = new FormData(event.currentTarget)
+                    // const token = String(formData.get("token"))
+                    const repoOwner = String(formData.get("repo-owner"))
+                    const repoName = String(formData.get("repo-name"))
 
-                setGitHubToken(token)
-                setGitHubRepo({ owner: repoOwner, name: repoName })
+                    // setGitHubToken(token)
+                    setGitHubRepo({ owner: repoOwner, name: repoName })
 
-                fetchNotes()
-                navigate("/")
-              }}
-            >
-              <div className="grid gap-2">
+                    fetchNotes()
+                    navigate("/")
+                  }}
+                >
+                  {/* <div className="grid gap-2">
                 <label htmlFor="auth-token" className="leading-4">
                   Personal access token
                 </label>
@@ -80,36 +63,93 @@ export function SettingsPage() {
                   </a>{" "}
                   with <code>repo</code> access, then paste it here.
                 </p>
+              </div> */}
+                  <div className="grid gap-2">
+                    <label htmlFor="repo-owner" className="leading-4">
+                      Repository owner
+                    </label>
+                    <Input
+                      id="repo-owner"
+                      name="repo-owner"
+                      spellCheck={false}
+                      defaultValue={githubRepo?.owner}
+                    />
+                  </div>
+                  <div className="grid gap-2">
+                    <label htmlFor="repo-name" className="leading-4">
+                      Repository name
+                    </label>
+                    <Input
+                      id="repo-name"
+                      name="repo-name"
+                      spellCheck={false}
+                      defaultValue={githubRepo?.name}
+                    />
+                  </div>
+                </form>
+                <Button form="github-form" type="submit" variant="primary" className="mt-2">
+                  Save
+                </Button>
               </div>
-              <div className="grid gap-2">
-                <label htmlFor="repo-owner" className="leading-4">
-                  Repository owner
-                </label>
-                <Input
-                  id="repo-owner"
-                  name="repo-owner"
-                  spellCheck={false}
-                  defaultValue={githubRepo?.owner}
-                />
-              </div>
-              <div className="grid gap-2">
-                <label htmlFor="repo-name" className="leading-4">
-                  Repository name
-                </label>
-                <Input
-                  id="repo-name"
-                  name="repo-name"
-                  spellCheck={false}
-                  defaultValue={githubRepo?.name}
-                />
-              </div>
-            </form>
-            <Button form="github-form" type="submit" variant="primary" className="mt-2">
-              Save
-            </Button>
+            ) : null}
           </Card>
         </div>
       </Panel>
     </>
   )
+}
+
+const githubUserSchema = z.object({
+  login: z.string(),
+  avatar_url: z.string(),
+})
+
+const githubUserAtom = atom<Promise<z.infer<typeof githubUserSchema> | null>>(async (get) => {
+  const token = get(githubTokenAtom)
+  if (!token) return null
+
+  // Fetch authenticated user
+  const response = await fetch("https://api.github.com/user", {
+    headers: {
+      Authorization: `token ${token}`,
+    },
+  })
+
+  if (!response.ok) return null
+
+  const user = githubUserSchema.parse(await response.json())
+
+  return user
+})
+
+function GitHubAuth() {
+  const [githubToken, setGitHubToken] = useAtom(githubTokenAtom)
+  const githubUser = useAtomValue(React.useMemo(() => loadable(githubUserAtom), []))
+
+  if (!githubToken) {
+    return (
+      <Button variant="primary" onClick={() => setGitHubToken(import.meta.env.VITE_GITHUB_TOKEN)}>
+        Sign in with GitHub
+      </Button>
+    )
+  }
+
+  return githubUser.state === "hasData" && githubUser.data ? (
+    <div className="flex items-center justify-between rounded-sm bg-bg-secondary p-4">
+      <div className="flex items-center gap-3">
+        <div
+          aria-hidden
+          className="inline-block h-8 w-8 rounded-full bg-bg-secondary bg-cover ring-1 ring-inset ring-border-secondary coarse:h-10 coarse:w-10"
+          style={{
+            backgroundImage: `url(${githubUser.data.avatar_url})`,
+          }}
+        />
+        <div className="flex flex-col gap-1">
+          <span className="text-sm leading-3 coarse:leading-4">Signed in as</span>
+          <span className="font-semibold leading-4 coarse:leading-5">{githubUser.data.login}</span>
+        </div>
+      </div>
+      <Button onClick={() => setGitHubToken("")}>Sign out</Button>
+    </div>
+  ) : null
 }
