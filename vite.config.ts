63be2040b8368a3fdea4e89d9781aa933fbb5ead
Felix Salazar <fxsalazar@gmail.com>
2023-07-20 22:53:40 +0200

    fix rollup-plugin-visualizer

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,12 @@ import react from "@vitejs/plugin-react"
 import jotaiDebugLabel from "jotai/babel/plugin-debug-label"
 import jotaiReactRefresh from "jotai/babel/plugin-react-refresh"
 import { visualizer } from "rollup-plugin-visualizer"
-import { defineConfig } from "vite"
+import { type PluginOption, defineConfig } from "vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react({ babel: { plugins: [jotaiDebugLabel, jotaiReactRefresh] } }),
-    visualizer({ filename: "dist/stats.html" }),
+    visualizer({ filename: "dist/stats.html" }) as unknown as PluginOption,
   ],
 })
