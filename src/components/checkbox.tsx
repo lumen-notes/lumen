da6b69f18c11f54a220a92e20c13dfde752a2456
Cole Bemis <colebemis@github.com>
2023-07-31 21:34:28 -0700

    Change border-radius of checkbox

--- src/components/checkbox.tsx
+++ src/components/checkbox.tsx
@@ -10,7 +10,7 @@ export const Checkbox = React.forwardRef<
   <CheckboxPrimitive.Root
     ref={ref}
     className={cx(
-      "flex h-4 w-4 shrink-0 items-center justify-center rounded-sm border border-border ring-offset-bg focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-border-focus focus-visible:ring-offset-1 disabled:opacity-50 data-[state=checked]:border-border-focus data-[state=checked]:bg-border-focus data-[state=checked]:text-bg data-[state=unchecked]:hover:bg-bg-secondary coarse:h-5 coarse:w-5",
+      "flex h-4 w-4 shrink-0 items-center justify-center rounded-xs border border-border ring-offset-bg focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-border-focus focus-visible:ring-offset-1 disabled:opacity-50 data-[state=checked]:border-border-focus data-[state=checked]:bg-border-focus data-[state=checked]:text-bg data-[state=unchecked]:hover:bg-bg-secondary coarse:h-5 coarse:w-5",
       className,
     )}
     {...props}
