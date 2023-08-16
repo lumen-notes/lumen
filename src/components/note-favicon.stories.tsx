cb5c7cb9cf49768fecdb5c9d2518013dd4f00700
Felix Salazar <fxsalazar@gmail.com>
2023-08-16 17:10:50 +0200

    Fix and test favicon for the note's list

---
+++ src/components/note-favicon.stories.tsx
@@ -0,0 +1,83 @@
+import { StoryObj } from "@storybook/react"
+import { Note } from "../types"
+import { Card } from "./card"
+import { NoteFavicon } from "./note-favicon"
+import { within } from "@storybook/testing-library"
+import { expect } from "@storybook/jest"
+
+export default {
+  title: "NoteFavicon",
+  component: NoteFavicon,
+  render: (args: { note: Note }) => (
+    <Card className="mx-auto max-w-lg p-4">
+      <NoteFavicon {...args} />
+    </Card>
+  ),
+}
+
+const noteStubbed: Note = {
+  backlinks: [],
+  dates: [],
+  frontmatter: {},
+  id: "1",
+  links: [],
+  queries: [],
+  tags: [],
+  rawBody: "",
+  tasks: [],
+  title: "",
+  url: null,
+}
+
+export const Github: StoryObj<typeof NoteFavicon> = {
+  args: {
+    note: {
+      ...noteStubbed,
+      frontmatter: {
+        github: "lumen-notes",
+      },
+    },
+  },
+  play: async ({ canvasElement }) => expectFavicon(canvasElement, "github-avatar"),
+}
+
+export const WithUrl: StoryObj<typeof NoteFavicon> = {
+  args: {
+    note: {
+      ...noteStubbed,
+      url: "https://example.com",
+    },
+  },
+  play: async ({ canvasElement }) => expectFavicon(canvasElement, "note-favicon-url"),
+}
+
+export const Book: StoryObj<typeof NoteFavicon> = {
+  args: {
+    note: {
+      ...noteStubbed,
+      frontmatter: {
+        isbn: 9781542866507,
+      },
+    },
+  },
+  play: async ({ canvasElement }) => expectFavicon(canvasElement, "note-favicon-book"),
+}
+
+export const Template: StoryObj<typeof NoteFavicon> = {
+  args: {
+    note: {
+      ...noteStubbed,
+      frontmatter: {
+        template: {
+          name: "name-template",
+        },
+      },
+    },
+  },
+  play: async ({ canvasElement }) => expectFavicon(canvasElement, "note-template-icon16"),
+}
+
+const expectFavicon = async (canvasElement: HTMLElement, favicon: string) => {
+  const canvas = within(canvasElement)
+  await expect(await canvas.findByTestId(favicon)).toBeTruthy()
+}
