5f3675aa484ead44d7864cd5bb42de8ca01a27c0
Cole Bemis <colebemis@github.com>
2023-09-06 22:16:22 -0700

    Create custom note node

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,18 +1,38 @@
-import ReactFlow, { Background, Controls } from "reactflow"
+import React from "react"
+import ReactFlow, { Background, Controls, NodeProps } from "reactflow"
 import "reactflow/dist/style.css"
+import { NoteCard } from "../components/note-card"
+import { NoteId } from "../types"
 
 const nodes = [
   {
     id: "1",
+    type: "note",
     position: { x: 0, y: 0 },
-    data: { label: "Node 1" },
+    data: { noteId: "1694032625739" },
   },
 ]
 
+function NoteNode({ data }: NodeProps<{ noteId: NoteId }>) {
+  return (
+    <div className="w-[500px]">
+      <NoteCard id={data.noteId} />
+    </div>
+  )
+}
+
 export function HomePage() {
+  const nodeTypes = React.useMemo(() => ({ note: NoteNode }), [])
+
   return (
     <div className="h-full">
-      <ReactFlow nodes={nodes} proOptions={{ hideAttribution: true }}>
+      <ReactFlow
+        nodeTypes={nodeTypes}
+        nodes={nodes}
+        proOptions={{
+          hideAttribution: true,
+        }}
+      >
         <Background />
         <Controls />
       </ReactFlow>
