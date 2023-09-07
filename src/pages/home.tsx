f4c19bea31685b84f096c65a9fa101425afcffcd
Cole Bemis <colebemis@github.com>
2023-09-06 22:30:05 -0700

    Interact with nodes

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,15 +1,29 @@
 import React from "react"
-import ReactFlow, { Background, Controls, NodeProps } from "reactflow"
+import ReactFlow, {
+  Background,
+  Controls,
+  Node,
+  NodeProps,
+  OnNodesChange,
+  SelectionMode,
+  applyNodeChanges,
+} from "reactflow"
 import "reactflow/dist/style.css"
 import { NoteCard } from "../components/note-card"
 import { NoteId } from "../types"
 
-const nodes = [
+const initialNodes = [
   {
     id: "1",
     type: "note",
     position: { x: 0, y: 0 },
     data: { noteId: "1694032625739" },
+  },
+  {
+    id: "2",
+    type: "note",
+    position: { x: 800, y: 400 },
+    data: { noteId: "1652342106359" },
   },
 ]
 
@@ -24,14 +38,21 @@ function NoteNode({ data }: NodeProps<{ noteId: NoteId }>) {
 export function HomePage() {
   const nodeTypes = React.useMemo(() => ({ note: NoteNode }), [])
 
+  const [nodes, setNodes] = React.useState<Node[]>(initialNodes)
+
+  const onNodesChange: OnNodesChange = React.useCallback(
+    (changes) => setNodes((n) => applyNodeChanges(changes, n)),
+    [],
+  )
+
   return (
     <div className="h-full">
       <ReactFlow
         nodeTypes={nodeTypes}
         nodes={nodes}
-        proOptions={{
-          hideAttribution: true,
-        }}
+        onNodesChange={onNodesChange}
+        selectionMode={SelectionMode.Partial}
+        proOptions={{ hideAttribution: true }}
       >
         <Background />
         <Controls />
