e43b447f222f0f8ad9cdc98decaedd0277f8e5ae
Cole Bemis <colebemis@github.com>
2023-10-07 02:48:24 +0300

    Comment out spellcheck and autocorrect

--- src/components/note-editor.tsx
+++ src/components/note-editor.tsx
@@ -108,7 +108,7 @@ export const NoteEditor = React.forwardRef<ReactCodeMirrorRef, NoteEditorProps>(
           }
         }}
         extensions={[
-          EditorView.contentAttributes.of({ spellcheck: "true" }),
+          // EditorView.contentAttributes.of({ spellcheck: "true", autocorrect: "on" }),
           EditorView.domEventHandlers({
             paste: (event, view) => {
               const clipboardText = event.clipboardData?.getData("text/plain") ?? ""
