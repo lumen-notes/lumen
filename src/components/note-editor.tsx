3e7b7672637b5d01355f9a5d1752e412932f411b
Cole Bemis <colebemis@github.com>
2023-10-08 05:06:30 +0300

    Implement frontmatter autocomplete (#184)
    * Implement frontmatter autocomplete
    
    * Remove unused import
    
    * Update cursor margin
    
    * Account for mobile autocomplete

--- src/components/note-editor.tsx
+++ src/components/note-editor.tsx
@@ -108,6 +108,30 @@ export const NoteEditor = React.forwardRef<ReactCodeMirrorRef, NoteEditorProps>(
           }
         }}
         extensions={[
+          EditorView.inputHandler.of((view: EditorView, from: number, to: number, text: string) => {
+            // If you're inserting a `-` at index 2 and all previous characters are also `-`,
+            // insert a matching `---` below the line
+            if (
+              (text === "-" && from === 2 && view.state.sliceDoc(0, 2) === "--") ||
+              // Sometimes the mobile Safari replaces `--` with `—` so we need to handle that case too
+              (text === "-" && from === 1 && view.state.sliceDoc(0, 1) === "—")
+            ) {
+              view.dispatch({
+                changes: {
+                  from: 0,
+                  to,
+                  insert: "---\n\n---",
+                },
+                selection: {
+                  anchor: 4,
+                },
+              })
+
+              return true
+            }
+
+            return false
+          }),
           EditorView.contentAttributes.of({ spellcheck: "true" }),
           EditorView.domEventHandlers({
             paste: (event, view) => {
