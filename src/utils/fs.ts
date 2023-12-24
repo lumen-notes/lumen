5776d1f24c1b7d54fbb31f592337812cebc06852
Noah Solomon <noahsolomon2003@gmail.com>
2023-12-23 21:08:28 -0500

    moving logFileSystemState to fs.ts

--- src/utils/fs.ts
+++ src/utils/fs.ts
@@ -7,6 +7,7 @@ import {
   resolveGitLfsPointer,
   uploadToGitLfsServer,
 } from "./git-lfs"
+import { REPO_DIR } from "../global-state"
 
 const DB_NAME = "fs"
 
@@ -77,5 +78,33 @@ export async function writeFile({
   } else {
     // TODO: Test this
     await fs.promises.writeFile(path, Buffer.from(content))
+  }
+}
+
+/** Log the state of the file system to the console */
+export async function logFileSystemState(fs: LightningFS, dir = REPO_DIR) {
+  try {
+    // List files and directories at the specified path
+    const files = await fs.promises.readdir(dir)
+
+    console.log(`Contents of ${dir}:`, files)
+
+    // Iterate over each file/directory
+    for (const file of files) {
+      // Ensure there is a slash between the directory and file names
+      const filePath = `${dir}/${file}`
+
+      const stats = await fs.promises.stat(filePath)
+      if (stats.isDirectory()) {
+        // If directory, recursively log its contents
+        await logFileSystemState(fs, `${filePath}/`)
+      } else {
+        // If file, read and log its contents
+        const content = await fs.promises.readFile(filePath, "utf8")
+        console.log(`Contents of ${filePath}:`, content)
+      }
+    }
+  } catch (error) {
+    console.error("Error logging file system state:", error)
   }
 }
