63e5d21cb2bd2ee179e2df767d815e0e7b3dd386
Cole Bemis <colebemis@github.com>
2023-12-25 01:26:43 -0800

    Remove proxy

--- netlify/functions/proxy.ts
+++
@@ -1,9 +0,0 @@
-import corsProxy from "@isomorphic-git/cors-proxy"
-import express from "express"
-import serverless from "serverless-http"
-
-const app = express()
-
-app.use(corsProxy)
-
-export const handler = serverless(app)
