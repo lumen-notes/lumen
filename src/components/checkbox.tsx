8a4a580d642ba9d3ae9d9481089414e32b4bf8b6
Cole Bemis <colebemis@github.com>
2023-12-25 17:46:38 -0800

    Remove unused priority styles

--- src/components/checkbox.tsx
+++ src/components/checkbox.tsx
@@ -3,25 +3,16 @@ import React from "react"
 import { cx } from "../utils/cx"
 import { CheckIcon16 } from "./icons"
 
-type CheckboxProps = CheckboxPrimitive.CheckboxProps & {
-  priority?: 1 | 2 | 3 | 4
-}
+type CheckboxProps = CheckboxPrimitive.CheckboxProps
 
 export const Checkbox = React.forwardRef<
   React.ElementRef<typeof CheckboxPrimitive.Root>,
   CheckboxProps
->(({ className, priority = 4, ...props }, ref) => (
+>(({ className, ...props }, ref) => (
   <CheckboxPrimitive.Root
     ref={ref}
-    style={{
-      // @ts-expect-error
-      "--hsl": `var(--hsl-priority-${priority})`,
-    }}
     className={cx(
-      "flex h-4 w-4 shrink-0 items-center justify-center rounded-sm border-2 border-[hsl(var(--hsl))] bg-[hsla(var(--hsl)_/_var(--a-bg-priority))] ring-offset-bg focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-border-focus focus-visible:ring-offset-1 disabled:opacity-50 data-[state=checked]:border-[hsl(var(--hsl))] data-[state=checked]:bg-[hsl(var(--hsl))] data-[state=checked]:text-bg data-[state=unchecked]:hover:bg-[hsla(var(--hsl)_/_var(--a-bg-priority-hover))] coarse:h-5 coarse:w-5",
-      // Default
-      priority === 4 &&
-        "border border-border bg-transparent data-[state=checked]:border-text-secondary data-[state=checked]:bg-text-secondary data-[state=unchecked]:hover:bg-bg-secondary",
+      "flex h-4 w-4 shrink-0 items-center justify-center rounded-sm border border-border bg-transparent ring-offset-bg focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-border-focus focus-visible:ring-offset-1 disabled:opacity-50 data-[state=checked]:border-border-focus data-[state=checked]:bg-border-focus data-[state=unchecked]:hover:bg-bg-secondary coarse:h-5 coarse:w-5",
       className,
     )}
     {...props}
