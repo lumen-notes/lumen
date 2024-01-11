b1d1b2890ceb8ab39f7f4d4db438c4f9094da37a
zaaakher <zakhermasri@gmail.com>
2024-01-11 03:46:39 +0300

    fix(shortcuts):  ctrl + k now works in windows

--- src/components/command-menu.tsx
+++ src/components/command-menu.tsx
@@ -70,7 +70,7 @@ export function CommandMenu() {
 
   // Toggle the menu with `command + k`
   useEvent("keydown", (event: KeyboardEvent) => {
-    if (event.key === "k" && event.metaKey) {
+    if (event.key === "k" && (event.metaKey || event.ctrlKey)) {
       if (isOpen) {
         closeMenu()
       } else {
