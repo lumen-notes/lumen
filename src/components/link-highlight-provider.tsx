0010783347b449ea9772f60ecd517dbce4c6ecdf
Cole Bemis <colebemis@github.com>
2024-02-25 10:50:00 -0800

    Highlight matching tags and links in search results

--- src/components/link-highlight-provider.tsx
+++ src/components/link-highlight-provider.tsx
@@ -1,21 +1,25 @@
 import React from "react"
 
 type LinkHighlightProviderProps = {
-  href: string
+  href: string | string[]
   children?: React.ReactNode
 }
 
 export function LinkHighlightProvider({ href, children }: LinkHighlightProviderProps) {
   // Scope styles using a unique ID
   const id = React.useMemo(() => `id-${Math.round(Math.random() * 10000000000)}`, [])
+  const hrefs = Array.isArray(href) ? href : [href]
   return (
     <div id={id}>
       <style>
-        {`#${id} a[href="${href}"] {
-          color: var(--color-text-highlight);
-          background-color: var(--color-bg-highlight);
-          text-decoration-color: currentColor;
-        }`}
+        {hrefs.map(
+          (href) =>
+            `#${id} a[href="${href}"] {
+              color: var(--color-text-highlight);
+              background-color: var(--color-bg-highlight);
+              text-decoration-color: currentColor;
+            }`,
+        )}
       </style>
       {children}
     </div>
--- src/components/note-list.tsx
+++ src/components/note-list.tsx
@@ -14,6 +14,7 @@ import { DropdownMenu } from "./dropdown-menu"
 import { IconButton } from "./icon-button"
 import { CardsIcon16, ListIcon16, TagIcon16 } from "./icons"
 import { Link } from "./link"
+import { LinkHighlightProvider } from "./link-highlight-provider"
 import { NoteCard } from "./note-card"
 import { NoteFavicon } from "./note-favicon"
 import { usePanel, usePanelActions } from "./panels"
@@ -97,170 +98,195 @@ export function NoteList({ baseQuery = "" }: NoteListProps) {
     )
   }, [noteResults])
 
-  const tagQualifiers = React.useMemo(() => {
-    return parseQuery(query).qualifiers.filter((qualifier) => qualifier.key === "tag")
+  const qualifiers = React.useMemo(() => {
+    return parseQuery(query).qualifiers
   }, [query])
 
+  const tagQualifiers = React.useMemo(() => {
+    return qualifiers.filter((qualifier) => qualifier.key === "tag")
+  }, [qualifiers])
+
+  const highlightPaths = React.useMemo(() => {
+    return qualifiers
+      .filter((qualifier) => !qualifier.exclude)
+      .flatMap((qualifier) => {
+        switch (qualifier.key) {
+          case "tag":
+            return qualifier.values.map((value) => `/tags/${value}`)
+          case "link":
+            return qualifier.values.map((value) => `/${value}`)
+          case "date":
+            return qualifier.values.map((value) => `/${value}`)
+          default:
+            return []
+        }
+      })
+  }, [qualifiers])
+
   return (
-    <div>
-      <div className="flex flex-col gap-4">
-        <div className="flex flex-col gap-2">
-          <div className="grid grid-cols-[1fr_auto_auto] gap-2">
-            <SearchInput
-              placeholder={`Search ${pluralize(noteResults.length, "note")}…`}
-              value={query}
-              onChange={(value) => {
-                setQuery(value)
+    <LinkHighlightProvider href={highlightPaths}>
+      <div>
+        <div className="flex flex-col gap-4">
+          <div className="flex flex-col gap-2">
+            <div className="grid grid-cols-[1fr_auto_auto] gap-2">
+              <SearchInput
+                placeholder={`Search ${pluralize(noteResults.length, "note")}…`}
+                value={query}
+                onChange={(value) => {
+                  setQuery(value)
 
-                // Reset the number of visible notes when the user starts typing
-                setNumVisibleNotes(10)
-              }}
-            />
-            <IconButton
-              aria-label={viewType === "cards" ? "List view" : "Card view"}
-              className="h-10 w-10 rounded-md bg-bg-secondary hover:bg-bg-tertiary coarse:h-12 coarse:w-12"
-              onClick={() => setViewType(viewType === "cards" ? "list" : "cards")}
-            >
-              {viewType === "cards" ? <ListIcon16 /> : <CardsIcon16 />}
-            </IconButton>
-            <DiceButton
-              onClick={() => {
-                const resultsCount = noteResults.length
-                const randomIndex = Math.floor(Math.random() * resultsCount)
-                openPanel?.(noteResults[randomIndex].id, panel?.index)
-              }}
-            />
+                  // Reset the number of visible notes when the user starts typing
+                  setNumVisibleNotes(10)
+                }}
+              />
+              <IconButton
+                aria-label={viewType === "cards" ? "List view" : "Card view"}
+                className="h-10 w-10 rounded-md bg-bg-secondary hover:bg-bg-tertiary coarse:h-12 coarse:w-12"
+                onClick={() => setViewType(viewType === "cards" ? "list" : "cards")}
+              >
+                {viewType === "cards" ? <ListIcon16 /> : <CardsIcon16 />}
+              </IconButton>
+              <DiceButton
+                onClick={() => {
+                  const resultsCount = noteResults.length
+                  const randomIndex = Math.floor(Math.random() * resultsCount)
+                  openPanel?.(noteResults[randomIndex].id, panel?.index)
+                }}
+              />
+            </div>
+            {query ? (
+              <span className="text-sm text-text-secondary">
+                {pluralize(noteResults.length, "result")}
+              </span>
+            ) : null}
           </div>
-          {query ? (
-            <span className="text-sm text-text-secondary">
-              {pluralize(noteResults.length, "result")}
-            </span>
-          ) : null}
-        </div>
-        <div className="flex flex-wrap gap-2 empty:hidden">
-          {sortedTagFrequencies.length > 0 || tagQualifiers.length > 0 ? (
-            <>
-              {tagQualifiers.map((qualifier) => (
-                <PillButton
-                  key={qualifier.values.join(",")}
-                  data-tag={qualifier.values.join(",")}
-                  variant="primary"
-                  removable
-                  onClick={() => {
-                    const text = `${qualifier.exclude ? "-" : ""}tag:${qualifier.values.join(",")}`
+          <div className="flex flex-wrap gap-2 empty:hidden">
+            {sortedTagFrequencies.length > 0 || tagQualifiers.length > 0 ? (
+              <>
+                {tagQualifiers.map((qualifier) => (
+                  <PillButton
+                    key={qualifier.values.join(",")}
+                    data-tag={qualifier.values.join(",")}
+                    variant="primary"
+                    removable
+                    onClick={() => {
+                      const text = `${qualifier.exclude ? "-" : ""}tag:${qualifier.values.join(
+                        ",",
+                      )}`
 
-                    const index = query.indexOf(text)
+                      const index = query.indexOf(text)
 
-                    if (index === -1) return
+                      if (index === -1) return
 
-                    const newQuery =
-                      query.slice(0, index) + query.slice(index + text.length).trimStart()
+                      const newQuery =
+                        query.slice(0, index) + query.slice(index + text.length).trimStart()
 
-                    // Remove the tag qualifier from the query
-                    setQuery(newQuery.trim())
+                      // Remove the tag qualifier from the query
+                      setQuery(newQuery.trim())
 
-                    // TODO: Move focus
-                  }}
-                >
-                  {qualifier.exclude ? <span>not</span> : null}
-                  {qualifier.values.map((value, index) => (
-                    <React.Fragment key={value}>
-                      {index > 0 ? <span>or</span> : null}
-                      <span key={value}>{value}</span>
-                    </React.Fragment>
-                  ))}
-                </PillButton>
-              ))}
-              {sortedTagFrequencies.slice(0, numVisibleTags).map(([tag, frequency]) => (
-                <PillButton
-                  key={tag}
-                  data-tag={tag}
-                  onClick={(event) => {
-                    const qualifier = `${event.shiftKey ? "-" : ""}tag:${tag}`
-
-                    flushSync(() => {
-                      setQuery(query ? `${query} ${qualifier}` : qualifier)
-                    })
-
-                    // Move focus
-                    document.querySelector<HTMLElement>(`[data-tag="${tag}"]`)?.focus()
-                  }}
-                >
-                  {tag}
-                  <span className="text-text-secondary">{frequency}</span>
-                </PillButton>
-              ))}
-              {sortedTagFrequencies.length > numVisibleTags ? (
-                <DropdownMenu>
-                  <DropdownMenu.Trigger asChild>
-                    <PillButton variant="dashed">Show more</PillButton>
-                  </DropdownMenu.Trigger>
-                  <DropdownMenu.Content>
-                    {sortedTagFrequencies.slice(numVisibleTags).map(([tag, frequency]) => (
-                      <DropdownMenu.Item
-                        key={tag}
-                        icon={<TagIcon16 />}
-                        trailingVisual={<span className="text-text-secondary">{frequency}</span>}
-                        onClick={(event) => {
-                          const qualifier = `${event.shiftKey ? "-" : ""}tag:${tag}`
-                          setQuery(query ? `${query} ${qualifier}` : qualifier)
-                        }}
-                      >
-                        {tag}
-                      </DropdownMenu.Item>
-                    ))}
-                  </DropdownMenu.Content>
-                </DropdownMenu>
-              ) : null}
-            </>
-          ) : null}
-        </div>
-        {viewType === "cards"
-          ? noteResults.slice(0, numVisibleNotes).map(({ id }) => <NoteCard key={id} id={id} />)
-          : null}
-        {viewType === "list" ? (
-          <ul>
-            {noteResults.slice(0, numVisibleNotes).map((note) => {
-              const parsedTemplate = templateSchema
-                .omit({ body: true })
-                .safeParse(note.frontmatter.template)
-              return (
-                // TODO: Move this into a NoteItem component
-                <li key={note.id}>
-                  <Link
-                    // Used for focus management
-                    data-note-id={note.id}
-                    to={`/${note.id}`}
-                    target="_blank"
-                    className="focus-ring flex gap-3 rounded-md p-3 leading-4 hover:bg-bg-secondary coarse:p-4"
+                      // TODO: Move focus
+                    }}
                   >
-                    <NoteFavicon note={note} />
-                    <span className="truncate text-text-secondary">
-                      <span className="text-text">
-                        {parsedTemplate.success
-                          ? `${parsedTemplate.data.name} template`
-                          : note.title || note.id}
+                    {qualifier.exclude ? <span>not</span> : null}
+                    {qualifier.values.map((value, index) => (
+                      <React.Fragment key={value}>
+                        {index > 0 ? <span>or</span> : null}
+                        <span key={value}>{value}</span>
+                      </React.Fragment>
+                    ))}
+                  </PillButton>
+                ))}
+                {sortedTagFrequencies.slice(0, numVisibleTags).map(([tag, frequency]) => (
+                  <PillButton
+                    key={tag}
+                    data-tag={tag}
+                    onClick={(event) => {
+                      const qualifier = `${event.shiftKey ? "-" : ""}tag:${tag}`
+
+                      flushSync(() => {
+                        setQuery(query ? `${query} ${qualifier}` : qualifier)
+                      })
+
+                      // Move focus
+                      document.querySelector<HTMLElement>(`[data-tag="${tag}"]`)?.focus()
+                    }}
+                  >
+                    {tag}
+                    <span className="text-text-secondary">{frequency}</span>
+                  </PillButton>
+                ))}
+                {sortedTagFrequencies.length > numVisibleTags ? (
+                  <DropdownMenu>
+                    <DropdownMenu.Trigger asChild>
+                      <PillButton variant="dashed">Show more</PillButton>
+                    </DropdownMenu.Trigger>
+                    <DropdownMenu.Content>
+                      {sortedTagFrequencies.slice(numVisibleTags).map(([tag, frequency]) => (
+                        <DropdownMenu.Item
+                          key={tag}
+                          icon={<TagIcon16 />}
+                          trailingVisual={<span className="text-text-secondary">{frequency}</span>}
+                          onClick={(event) => {
+                            const qualifier = `${event.shiftKey ? "-" : ""}tag:${tag}`
+                            setQuery(query ? `${query} ${qualifier}` : qualifier)
+                          }}
+                        >
+                          {tag}
+                        </DropdownMenu.Item>
+                      ))}
+                    </DropdownMenu.Content>
+                  </DropdownMenu>
+                ) : null}
+              </>
+            ) : null}
+          </div>
+          {viewType === "cards"
+            ? noteResults.slice(0, numVisibleNotes).map(({ id }) => <NoteCard key={id} id={id} />)
+            : null}
+          {viewType === "list" ? (
+            <ul>
+              {noteResults.slice(0, numVisibleNotes).map((note) => {
+                const parsedTemplate = templateSchema
+                  .omit({ body: true })
+                  .safeParse(note.frontmatter.template)
+                return (
+                  // TODO: Move this into a NoteItem component
+                  <li key={note.id}>
+                    <Link
+                      // Used for focus management
+                      data-note-id={note.id}
+                      to={`/${note.id}`}
+                      target="_blank"
+                      className="focus-ring flex gap-3 rounded-md p-3 leading-4 hover:bg-bg-secondary coarse:p-4"
+                    >
+                      <NoteFavicon note={note} />
+                      <span className="truncate text-text-secondary">
+                        <span className="text-text">
+                          {parsedTemplate.success
+                            ? `${parsedTemplate.data.name} template`
+                            : note.title || note.id}
+                        </span>
+                        <span className="ml-2 ">
+                          {removeParentTags(note.tags)
+                            .map((tag) => `#${tag}`)
+                            .join(" ")}
+                        </span>
                       </span>
-                      <span className="ml-2 ">
-                        {removeParentTags(note.tags)
-                          .map((tag) => `#${tag}`)
-                          .join(" ")}
-                      </span>
-                    </span>
-                  </Link>
-                </li>
-              )
-            })}
-          </ul>
+                    </Link>
+                  </li>
+                )
+              })}
+            </ul>
+          ) : null}
+        </div>
+
+        {noteResults.length > numVisibleNotes ? (
+          <Button ref={bottomRef} className="mt-4 w-full" onClick={loadMore}>
+            Load more
+          </Button>
         ) : null}
       </div>
-
-      {noteResults.length > numVisibleNotes ? (
-        <Button ref={bottomRef} className="mt-4 w-full" onClick={loadMore}>
-          Load more
-        </Button>
-      ) : null}
-    </div>
+    </LinkHighlightProvider>
   )
 }
 
