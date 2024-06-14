bd1349f1f8545794116b83d49a6b89cf5337921d
Pedro Carreño <34664891+Pkcarreno@users.noreply.github.com>
2024-06-14 18:38:38 -0400

    chore(config): migrate manifest and workbox to VitePWA (#361)
    Co-authored-by: Cole Bemis <colebemis@github.com>

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,10 @@ import react from "@vitejs/plugin-react"
 import jotaiDebugLabel from "jotai/babel/plugin-debug-label"
 import jotaiReactRefresh from "jotai/babel/plugin-react-refresh"
 import { visualizer } from "rollup-plugin-visualizer"
-import { defineConfig, type PluginOption } from "vite"
+import { defineConfig } from "vite"
+import type { PluginOption } from "vite"
 import { nodePolyfills } from "vite-plugin-node-polyfills"
+import { VitePWA } from "vite-plugin-pwa"
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,6 +14,40 @@ export default defineConfig({
     visualizer({ filename: "dist/stats.html" }) as unknown as PluginOption,
     // Fixes isomorphic-git Buffer error
     // https://github.com/isomorphic-git/isomorphic-git/issues/1753
+    VitePWA({
+      strategies: "generateSW",
+      registerType: "prompt",
+      injectRegister: "auto",
+      manifest: {
+        name: "Lumen",
+        short_name: "Lumen",
+        description: "A simple note-taking app for better thinking",
+        theme_color: "",
+        icons: [
+          {
+            src: "icon-1024.png",
+            sizes: "1024x1024",
+            type: "image/png",
+          },
+        ],
+        start_url: "/",
+        display: "standalone",
+      },
+      workbox: {
+        globPatterns: ["**/*.{html,css,js,woff2}"],
+        ignoreURLParametersMatching: [/^utm_/, /^fbclid$/],
+        skipWaiting: true,
+        navigateFallback: "index.html",
+        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024, // 5MB
+        sourcemap: true,
+        // Do not cache edge function routes
+        navigateFallbackDenylist: [/cors-proxy/, /git-lfs-file/, /github-auth/],
+      },
+      devOptions: {
+        enabled: process.env.NODE_ENV === "development",
+        type: "module",
+      },
+    }),
     nodePolyfills(),
   ],
 })
