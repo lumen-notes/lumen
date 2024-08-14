2ce9978b152ef78506f66fa4a7dcf5b7ce25731c
Cole Bemis <colebemis@github.com>
2024-08-14 12:15:49 -0700

    Remove refresh icon

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -667,24 +667,6 @@ export function UndoIcon16(props: IconProps) {
   )
 }
 
-export function RefreshIcon24(props: IconProps) {
-  return (
-    <Icon
-      size={24}
-      fill="none"
-      stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      {...props}
-    >
-      <polyline points="23 4 23 10 17 10" />
-      <polyline points="1 20 1 14 7 14" />
-      <path d="M3.51 9a9 9 0 0 1 14.85-3.36L23 10M1 14l4.64 4.36A9 9 0 0 0 20.49 15" />
-    </Icon>
-  )
-}
-
 // Brand icons
 
 export function GitHubIcon16() {
