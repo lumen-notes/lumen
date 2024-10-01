6bb655fed2aa6c87b0fc6316c8c0019c7fe86885
Cole Bemis <colebemis@github.com>
2024-10-01 13:17:26 -0700

    Update welcome note

--- src/utils/sample-markdown-files.ts
+++ src/utils/sample-markdown-files.ts
@@ -11,7 +11,7 @@ Lumen is a simple note-taking app for capturing and organizing your thoughts.
 
 - Write notes in markdown files and store them in a GitHub repository of your choice. You stay in control of your data.
 - Connect your notes with links and tags. Lumen makes it easy to explore  your knowledge graph and discover new connections.
-- Access your notes from any device, even offline. Use Lumen's powerful search to find exactly what you're looking for.
+- Access your notes from any device, even offline. Use Lumen's flexible query language to find exactly what you're looking for.
 
 Sign in with GitHub to get started.`,
   }
