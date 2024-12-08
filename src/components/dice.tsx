9a99741e1c4bdfa9a15ed0da2cd29d3ead7358e4
Cole Bemis <colebemis@github.com>
2024-12-08 11:23:40 -0800

    Update icons

--- src/components/dice.tsx
+++ src/components/dice.tsx
@@ -20,7 +20,7 @@ export function Dice({
         "--shadow-large": "calc(var(--x-amount) * 4px) calc(var(--y-amount) * 4px) 6px -1px",
       }}
       className={cx(
-        "flex rotate-[var(--angle)] rounded-[2px] bg-bg text-text transition-[transform,box-shadow] duration-200 will-change-transform [box-shadow:var(--shadow-large)_rgb(0,0,0,0.1),var(--shadow-small)_rgb(0,0,0,0.1),0_0_0_1px_var(--color-border-secondary)] motion-reduce:transition-none dark:bg-text-secondary dark:text-bg-inset dark:[box-shadow:inset_calc(var(--x-amount)*1px)_calc(var(--y-amount)*1px)_0_var(--color-border),var(--shadow-large)_rgb(0,0,0,0.2),var(--shadow-small)_rgb(0,0,0,0.2)]",
+        "flex rotate-[var(--angle)] rounded-[2.5px] bg-bg text-text transition-[transform,box-shadow] duration-200 will-change-transform [box-shadow:var(--shadow-large)_rgb(0,0,0,0.1),var(--shadow-small)_rgb(0,0,0,0.1),0_0_0_1px_var(--color-border-secondary)] motion-reduce:transition-none dark:bg-text-secondary dark:text-bg-inset dark:[box-shadow:inset_calc(var(--x-amount)*1px)_calc(var(--y-amount)*1px)_0_var(--color-border),var(--shadow-large)_rgb(0,0,0,0.2),var(--shadow-small)_rgb(0,0,0,0.2)]",
         className,
       )}
     >
@@ -31,45 +31,45 @@ export function Dice({
         fill="currentColor"
         // className="dark:[filter:drop-shadow(calc(var(--x-amount)*1px)_calc(var(--y-amount)*1px)_0_var(--color-border))]"
       >
-        {number === 1 && <rect x="7" y="7" width="2" height="2" />}
+        {number === 1 && <circle cx="8" cy="8" r="1" />}
         {number === 2 && (
           <>
-            <rect x="4" y="4" width="2" height="2" />
-            <rect x="10" y="10" width="2" height="2" />
+            <circle cx="4" cy="4" r="1" />
+            <circle cx="12" cy="12" r="1" />
           </>
         )}
         {number === 3 && (
           <>
-            <rect x="4" y="4" width="2" height="2" />
-            <rect x="7" y="7" width="2" height="2" />
-            <rect x="10" y="10" width="2" height="2" />
+            <circle cx="4" cy="4" r="1" />
+            <circle cx="8" cy="8" r="1" />
+            <circle cx="12" cy="12" r="1" />
           </>
         )}
         {number === 4 && (
           <>
-            <rect x="4" y="4" width="2" height="2" />
-            <rect x="4" y="10" width="2" height="2" />
-            <rect x="10" y="4" width="2" height="2" />
-            <rect x="10" y="10" width="2" height="2" />
+            <circle cx="4" cy="4" r="1" />
+            <circle cx="4" cy="12" r="1" />
+            <circle cx="12" cy="4" r="1" />
+            <circle cx="12" cy="12" r="1" />
           </>
         )}
         {number === 5 && (
           <>
-            <rect x="4" y="4" width="2" height="2" />
-            <rect x="4" y="10" width="2" height="2" />
-            <rect x="7" y="7" width="2" height="2" />
-            <rect x="10" y="4" width="2" height="2" />
-            <rect x="10" y="10" width="2" height="2" />
+            <circle cx="4" cy="4" r="1" />
+            <circle cx="4" cy="12" r="1" />
+            <circle cx="8" cy="8" r="1" />
+            <circle cx="12" cy="4" r="1" />
+            <circle cx="12" cy="12" r="1" />
           </>
         )}
         {number === 6 && (
           <>
-            <rect x="4" y="4" width="2" height="2" />
-            <rect x="4" y="7" width="2" height="2" />
-            <rect x="4" y="10" width="2" height="2" />
-            <rect x="10" y="4" width="2" height="2" />
-            <rect x="10" y="7" width="2" height="2" />
-            <rect x="10" y="10" width="2" height="2" />
+            <circle cx="4" cy="4" r="1" />
+            <circle cx="4" cy="8" r="1" />
+            <circle cx="4" cy="12" r="1" />
+            <circle cx="12" cy="4" r="1" />
+            <circle cx="12" cy="8" r="1" />
+            <circle cx="12" cy="12" r="1" />
           </>
         )}
       </svg>
--- src/components/sync-status.tsx
+++ src/components/sync-status.tsx
@@ -6,9 +6,10 @@ import {
   CheckIcon12,
   CheckIcon8,
   CloseIcon12,
-  CloseIcon8,
+  XIcon8,
   OfflineIcon16,
   OfflineIcon24,
+  LoadingIcon12,
 } from "./icons"
 import { IconButton, IconButtonProps } from "./icon-button"
 
@@ -103,10 +104,8 @@ function SuccessIcon24() {
 
 function PendingIcon16() {
   return (
-    <div className="rounded-full bg-[var(--yellow-11)] text-bg">
-      <svg className="animate-spin" viewBox="0 0 16" width="16" height="16" fill="currentColor">
-        <path d="M8 3a5 5 0 1 0 5 5h-1.5A3.5 3.5 0 1 1 8 4.5V3Z" />
-      </svg>
+    <div className="grid h-4 w-4 place-items-center rounded-full bg-[var(--yellow-11)] text-bg">
+      <LoadingIcon12 />
     </div>
   )
 }
@@ -124,7 +123,7 @@ function PendingIcon24() {
 function ErrorIcon16() {
   return (
     <div className="grid h-4 w-4 place-items-center rounded-full bg-[var(--red-11)] text-bg">
-      <CloseIcon8 />
+      <XIcon8 />
     </div>
   )
 }
