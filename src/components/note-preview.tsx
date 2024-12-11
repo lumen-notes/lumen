210cef96cfc51d6d6a0dfdff47252ac23cc4a96a
Cole Bemis <colebemis@github.com>
2024-12-11 11:38:55 -0800

    Polish

--- src/components/note-preview.tsx
+++ src/components/note-preview.tsx
@@ -2,7 +2,7 @@ import { Markdown } from "./markdown"
 
 export function NotePreview({ children }: { children: string }) {
   return (
-    <div className="aspect-[5/3] w-full overflow-hidden p-3 [contain:layout_paint] [mask-image:linear-gradient(to_bottom,black_0%,black_80%,transparent_100%)] [&_*::-webkit-scrollbar]:hidden">
+    <div className="aspect-[5/3] w-full overflow-hidden p-3 [contain:layout_paint] [mask-image:linear-gradient(to_bottom,black_0%,black_75%,transparent_100%)] [&_*::-webkit-scrollbar]:hidden">
       <div {...{ inert: "" }} className="w-[125%] origin-top-left scale-[80%]">
         <Markdown hideFrontmatter>{children}</Markdown>
       </div>
