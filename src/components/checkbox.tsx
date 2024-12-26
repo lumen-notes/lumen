134045249c78c99d4433a7ad89871a04e6c6444d
Cole Bemis <colebemis@github.com>
2024-12-25 21:00:21 -0800

    Update checkboxes on touch

--- src/components/checkbox.tsx
+++ src/components/checkbox.tsx
@@ -12,13 +12,13 @@ export const Checkbox = React.forwardRef<
   <CheckboxPrimitive.Root
     ref={ref}
     className={cx(
-      "flex size-icon shrink-0 items-center justify-center rounded-sm border border-border bg-transparent focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-1 focus-visible:outline-border-focus disabled:opacity-50 data-[state=checked]:border-border-focus data-[state=checked]:bg-border-focus data-[state=unchecked]:hover:bg-bg-secondary",
+      "flex size-4 shrink-0 items-center justify-center rounded-sm border border-border bg-transparent focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-1 focus-visible:outline-border-focus disabled:opacity-50 data-[state=checked]:border-border-focus data-[state=checked]:bg-border-focus data-[state=unchecked]:hover:bg-bg-secondary coarse:size-5",
       className,
     )}
     {...props}
   >
     <CheckboxPrimitive.Indicator>
-      <CheckIcon8 className="text-bg" />
+      <CheckIcon8 className="text-bg coarse:size-2.5" />
     </CheckboxPrimitive.Indicator>
   </CheckboxPrimitive.Root>
 ))
