af926da1488703e9c4843d65e00f522b06daa14e
Cole Bemis <colebemis@github.com>
2025-05-20 21:52:06 -0700

    Update AssistantActivityIndicator stories to include undefined stream prop

--- src/components/assistant-activity-indicator.stories.tsx
+++ src/components/assistant-activity-indicator.stories.tsx
@@ -22,7 +22,7 @@ type Story = StoryObj<typeof AssistantActivityIndicator>
 export const Thinking: Story = {
   render: (args) => {
     return (
-      <AssistantActivityIndicator state={args.state}>
+      <AssistantActivityIndicator state={args.state} stream={undefined}>
         <div style={{ width: "64px", height: "32px" }} />
       </AssistantActivityIndicator>
     )
@@ -35,7 +35,7 @@ export const Thinking: Story = {
 export const Speaking: Story = {
   render: (args) => {
     return (
-      <AssistantActivityIndicator state={args.state}>
+      <AssistantActivityIndicator state={args.state} stream={undefined}>
         <div style={{ width: "64px", height: "32px" }} />
       </AssistantActivityIndicator>
     )
